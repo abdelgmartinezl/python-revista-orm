ba287230fbb11137fb06c8c4a152805799d90aa3
Abdel Gadiel Martinez Lassonde <abdel.g.martinez.l@gmail.com>
2024-02-05 21:42:31 -0500

    Segunda version de 1:1, 1:M, M:M en SQLAlchemy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Table
-from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-import mysql.connector
+from sqlalchemy.orm import relationship, sessionmaker
 
 # Conexion a la base de datos
 DATABASE_URL = "mysql+mysqlconnector://root@localhost:3306/revista"
@@ -10,6 +9,12 @@ engine = create_engine(DATABASE_URL, echo=False)
 # Clase base para trabajar con SQLAlchemy
 Base = declarative_base()
 
+# Relacion M:M
+association_table = Table('user_group_association', Base.metadata,
+                          Column('user_id', Integer, ForeignKey('users.id')),
+                          Column('group_id', Integer, ForeignKey('groups.id'))
+                          )
+
 # Relacion 1:1
 class User(Base):
     __tablename__ = "users"
@@ -17,6 +22,8 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     username = Column(String(50))
     address = relationship("Address", uselist=False, back_populates="user")
+    articles = relationship("Article", back_populates="author", uselist=True)
+    groups = relationship("Group", secondary=association_table, back_populates="users", uselist=True)
 
 class Address(Base):
     __tablename__ = "addresses"
@@ -33,35 +40,15 @@ class Article(Base):
     id = Column(Integer, primary_key=True)
     title = Column(String(50))
     content = Column(String(150))
-    author_id = Column(Integer, ForeignKey('users_one_to_many.id'))
-    author = relationship("UserOneToMany", back_populates="articles")
-
-class UserOneToMany(Base):
-    __tablename__ = "users_one_to_many"
-
-    id = Column(Integer, primary_key=True)
-    username = Column(String(50))
-    articles = relationship("Article", back_populates="author")
-
-# Relacion M:M
-association_table = Table('user_group_association', Base.metadata,
-                          Column('user_id', Integer, ForeignKey('users_many_to_many.id')),
-                          Column('group_id', Integer, ForeignKey('groups.id'))
-                          )
-
-class UserManyToMany(Base):
-    __tablename__ = "users_many_to_many"
-
-    id = Column(Integer, primary_key=True)
-    username = Column(String(50))
-    groups = relationship("Group", secondary=association_table, back_populates="users")
+    author_id = Column(Integer, ForeignKey('users.id'))
+    author = relationship("User", back_populates="articles", uselist=True)
 
 class Group(Base):
     __tablename__ = "groups"
 
     id = Column(Integer, primary_key=True)
     name = Column(String(50))
-    users = relationship("UserManyToMany", secondary=association_table, back_populates="groups")
+    users = relationship("User", secondary=association_table, back_populates="groups", uselist=True)
 
 # Crear estructuras en la base de datos
 Base.metadata.create_all(engine)
@@ -71,26 +58,21 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 # Insertar datos 1:1
-user_one_to_one = User(username='petra')
-address_one_to_one = Address(email='petra@ejemplo.com', user=user_one_to_one)
-user_one_to_one.address = address_one_to_one
+user1 = User(username='petra')
+address1 = Address(email='petra@ejemplo.com', user=user1)
+user1.address = address1
 
 # Insertar datos 1:M
-user_one_to_many = UserOneToMany(username='calixtra',
-                                 articles=[
-                                     Article(title='Articulo X', content='Contenido de articulo X'),
-                                     Article(title='Articulo Y', content='Contenido de articulo Y')
-                                 ])
+user2 = User(username='calixtra')
+articulo1 = Article(title='Articulo X', content='Contenido de articulo X', author_id=user2.id)
+articulo2 = Article(title='Articulo Y', content='Contenido de articulo Y', author_id=user2.id)
 
 # Insertar datos M:M
-user_many_to_many_1 = UserManyToMany(username="susana")
-user_many_to_many_2 = UserManyToMany(username="toribia")
-group = Group(name="deudoras",
-              users=[user_many_to_many_1,user_many_to_many_2])
+user3 = User(username='susana')
+user4 = User(username='toribia')
+group1 = Group(name="deudoras", users=[user3, user4])
 
-session.add_all([user_one_to_one, user_one_to_many,
-                 user_many_to_many_1, user_many_to_many_2,
-                 group])
+session.add_all([user1, address1, user2, articulo1, articulo2, user3, user4, group1])
 
 session.commit()
 
@@ -101,9 +83,9 @@ for user in users:
     print(user.username)
 
 print("Relacion 1:M")
-articles = session.query(UserOneToMany).all()
+articles = session.query(Article).all()
 for article in articles:
-    print(article.username)
+    print(article.title)
 
 print("Relacion M:M")
 groups = session.query(Group).all()
@@ -117,7 +99,7 @@ print(user_query.username)
 
 # Consultar con join
 print("Consulta con Join")
-article_query = session.query(Article).join(UserOneToMany).filter(UserOneToMany.username == 'calixtra').all()
+article_query = session.query(Article).join(User).filter(User.username == 'calixtra').all()
 for article in article_query:
     print(article.title, article.content)
 
